00d2ab3d45ba9af815af5d11c291c9a704b6ddef
unknown <sam.junghwan@gmail.com>
2019-03-04 14:25:09 -0800

    My Test Project

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementByName("orange").innerHTML = "Do you have a job opportunity in the U.S.?";
+document.getElementById("orange1").innerHTML = "Already in U.S.?";
+document.getElementById("orange2").innerHTML = "In legal Status";
+document.getElementById("orange3").innerHTML = "Working on H1, L1, etc";
+document.getElementById("five").innerHTML = "Do you have a job opportunity in the U.S.?";
+document.getElementById("six").innerHTML = "Do you have a job opportunity in the U.S.?";
+
+
+<script src="script.js"></script>
